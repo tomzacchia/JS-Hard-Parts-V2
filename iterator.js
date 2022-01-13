3dbecb683445d6518e968b10827d2d1ba91b0ea6
tomzacchia <tomzacchia@live.com>
2022-01-13 13:38:16 -0500

    Add iterator solutions

---
+++ iterator.js
@@ -0,0 +1,159 @@
+// Type JavaScript here and click "Run Code" or press Ctrl + s
+console.log("Hello, world!");
+
+// CHALLENGE 1
+
+function sumFunc(arr) {
+  // YOUR CODE HERE
+}
+
+// Uncomment the lines below to test your work
+// const array = [1, 2, 3, 4];
+// console.log(sumFunc(array)); // -> should log 10
+
+function returnIterator(arr) {
+  let index = 0;
+
+  return function () {
+    if (index > arr.length - 1) return "out of bounds";
+
+    const element = arr[index];
+    index++;
+    return element;
+  };
+}
+
+// Uncomment the lines below to test your work
+// const array2 = ['a', 'b', 'c', 'd'];
+// const myIterator = returnIterator(array2);
+// console.log(myIterator()); // -> should log 'a'
+// console.log(myIterator()); // -> should log 'b'
+// console.log(myIterator()); // -> should log 'c'
+// console.log(myIterator()); // -> should log 'd'
+
+// CHALLENGE 2
+
+function nextIterator(arr) {
+  // YOUR CODE HERE
+  let index = 0;
+
+  return {
+    next: function () {
+      if (index > arr.length - 1) return undefined;
+
+      const element = arr[index];
+      index++;
+      return element;
+    },
+  };
+}
+
+// Uncomment the lines below to test your work
+// const array3 = [1, 2, 3];
+// const iteratorWithNext = nextIterator(array3);
+// console.log(iteratorWithNext.next()); // -> should log 1
+// console.log(iteratorWithNext.next()); // -> should log 2
+// console.log(iteratorWithNext.next()); // -> should log 3
+
+// CHALLENGE 3
+
+function sumArray(arr) {
+  let sum = 0;
+
+  const iteratorWithNext = nextIterator(arr);
+
+  for (let i = 0; i <= arr.length - 1; i++) {
+    sum += iteratorWithNext.next();
+  }
+
+  return sum;
+}
+
+// Uncomment the lines below to test your work
+// const array4 = [1, 2, 3, 4];
+// console.log(sumArray(array4)); // -> should log 10
+
+// CHALLENGE 4
+
+function setIterator(set) {
+  // YOUR CODE HERE
+}
+
+// Uncomment the lines below to test your work
+// const mySet = new Set('hey');
+// const iterateSet = setIterator(mySet);
+// console.log(iterateSet.next()); // -> should log 'h'
+// console.log(iterateSet.next()); // -> should log 'e'
+// console.log(iterateSet.next()); // -> should log 'y'
+
+// CHALLENGE 5
+
+function indexIterator(arr) {
+  // YOUR CODE HERE
+}
+
+// Uncomment the lines below to test your work
+// const array5 = ['a', 'b', 'c', 'd'];
+// const iteratorWithIndex = indexIterator(array5);
+// console.log(iteratorWithIndex.next()); // -> should log [0, 'a']
+// console.log(iteratorWithIndex.next()); // -> should log [1, 'b']
+// console.log(iteratorWithIndex.next()); // -> should log [2, 'c']
+
+// CHALLENGE 6
+
+function Words(string) {
+  this.str = string;
+}
+
+Words.prototype[Symbol.iterator] = function () {
+  let index = 0;
+  const wordsArr = this.str.split(" ");
+  let isDone = false;
+
+  return {
+    next: () => {
+      if (index > wordsArr.length - 1) {
+        return {
+          value: undefined,
+          done: true,
+        };
+      }
+
+      const element = wordsArr[index];
+      index++;
+
+      return {
+        value: element,
+        done: isDone,
+      };
+    },
+  };
+};
+
+// Uncomment the lines below to test your work
+const helloWorld = new Words("Hello World");
+for (let word of helloWorld) {
+  console.log(word);
+} // -> should log 'Hello' and 'World'
+
+// CHALLENGE 7
+
+function valueAndPrevIndex(array) {}
+
+const returnedSentence = valueAndPrevIndex([4, 5, 6]);
+console.log(returnedSentence.sentence());
+console.log(returnedSentence.sentence());
+console.log(returnedSentence.sentence());
+
+//CHALLENGE 8
+
+function* createConversation(string) {}
+
+console.log(createConversation("english").next());
+
+//CHALLENGE 9
+function waitForVerb(noun) {}
+
+async function f(noun) {}
+
+f("dog");
